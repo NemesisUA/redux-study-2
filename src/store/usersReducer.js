c97c26128ca777f561444a65a0af06116afaeb6e
NemesisUA <display.n0ne.01.gmail.com>
2023-07-25 13:28:31 +0300

    feat: 3 work with arrays

--- src/store/usersReducer.js
+++ src/store/usersReducer.js
@@ -5,10 +5,10 @@ const initialState = {
 export const usersReducer = (state = initialState, action) => {
   switch(action.type) {
       case 'ADD_USER':
-          return  {...state, cash: state.cash + action.payload}
+          return  {...state, users: [...state.users, action.payload]}
 
-      case 'GET_USERS':
-          return  {...state, cash: state.cash - action.payload}            
+      case 'DELETE_USER':
+          return  {...state, users: state.users.filter(user => user.id !== action.payload)}            
           
       default:
           return state;
